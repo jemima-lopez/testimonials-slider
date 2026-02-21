781e986b01c7a9d3190134f7f2ba52fb63242d36
Jemima <jlopezj11@miumg.edu.gt>
2026-02-20 21:33:54 -0600

    commit init JS

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const slides = document.querySelectorAll('.card1');
+
+//Navegation buttons
+const tanyaBtn = document.getElementById('tanya-btn');
+const johnBtn = document.getElementById('john-btn');
