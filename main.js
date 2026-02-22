9707c7f85a47b22291cb767e4e7989780981feb5
Jemima <jlopezj11@miumg.edu.gt>
2026-02-21 22:22:50 -0600

    commit js 1

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-
+//Slides
 const slides = document.querySelectorAll('.card1');
 
 //Navegation buttons
 const tanyaBtn = document.getElementById('tanya-btn');
 const johnBtn = document.getElementById('john-btn');
+
+const buttons = [tanyaBtn, johnBtn];
+//Current slide
+let currentSlide = 0;
+
+//Function to show the current slide
+function showSlide(index) 
+{ 
+    if(index < 0 || index >= slides.length) return; 
+    slides.forEach(slide => slide.style.display = 'none');
+});
