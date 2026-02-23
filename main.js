7bd9e2fa7489dcd1a189e5fa4cc061d25a02272c
Jemima <jlopezj11@miumg.edu.gt>
2026-02-22 23:18:00 -0600

    commit js 2

--- main.js
+++ main.js
@@ -6,12 +6,36 @@ const tanyaBtn = document.getElementById('tanya-btn');
 const johnBtn = document.getElementById('john-btn');
 
 const buttons = [tanyaBtn, johnBtn];
-//Current slide
+
 let currentSlide = 0;
 
-//Function to show the current slide
+//Function to show the slide based on index
 function showSlide(index) 
 { 
     if(index < 0 || index >= slides.length) return; 
-    slides.forEach(slide => slide.style.display = 'none');
+    slides.forEach(slide => {slide.style.display = 'none';
 });
+
+//Remove active class from all buttons
+buttons.forEach(btn => {
+    btn.classList.remove("active");
+});
+
+slides[index].style.display = "flex";
+
+buttons[index].classList.add("active");
+
+currentIndex = index;
+}
+
+//Event listeners for buttons
+tanyaBtn.addEventListener("click", () => {
+    showSlide(0);
+});
+
+johnBtn.addEventListener("click", () => {
+    showSlide(1);
+});
+
+showSlide(0);
+
